a875fff4ec2796b2dc9479a195a5016faa0f2635
Kyle Mistele <kyle@mistele.com>
2021-04-22 16:55:53 -0500

    Added test file and building out protocol

--- silence/request.go
+++ silence/request.go
@@ -3,7 +3,14 @@ package silence
 import (
 	"encoding/binary"
 	"fmt"
+	"math/rand"
 )
+
+type SilenceMessage interface {
+	Marshall() []byte
+	Unmarshall([]byte)
+}
+
 // RequestMessageType DEFINES TYPE CODES FOR SILENCE REQUESTS
 type RequestMessageType uint8
 const (
@@ -17,8 +24,12 @@ const (
 	ErrorGoBackToMessage	RequestMessageType = 0xF1
 
 )
+////////////////////////////////////////////////////////////////////
+// RequestMessage type
+////////////////////////////////////////////////////////////////////
 
 // RequestMessage IS THE MESSAGE FOR A SILENCE REQUEST TO THE SERVER
+// IMPLEMENTS SilenceMessage
 type RequestMessage struct {
 	Type 				RequestMessageType	// PROTOCOL MESSAGE TYPE
 	SequenceNumber		uint8				// SEQUENCE NUMBER, GOES 0->1->...255->0->1...
@@ -26,30 +37,86 @@ type RequestMessage struct {
 	Body				RequestMessageBody	// A REQUEST MESSAGE BODY, DEPENDING ON WHAT THE BODY IS
 }
 
-// RequestMessageBody DEFINES AN INTERFACE FOR DIFFERENT REQUEST MESSAGE BODY TYPES
-type RequestMessageBody interface {
-	Marshall() []byte
-}
-
 // Marshall WILL BUILD OUT THE RequestMessage INTO A STRING OF BYTES, PERFORMING ENCODING AS APPROPRIATE
 func (r *RequestMessage) Marshall() []byte {
 
-	headerBytes := make([]byte, 6)	// 6 BYTES FOR 48 BITS
+	headerBytes := make([]byte, 2)	// 6 BYTES FOR 48 BITS
 	headerBytes[0] = uint8(r.Type)
 	headerBytes[1] = r.SequenceNumber
 
-	binary.LittleEndian.PutUint32(headerBytes[2:6], r.Nonce)
+	nonceBytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(nonceBytes, r.Nonce)
+	headerBytes = append(headerBytes, nonceBytes...)
+
+	bodyBytes := r.Body.Marshall()
+	// TODO: XOR THIS WITH THE NONCE
+	headerBytes = append(headerBytes, bodyBytes...)
+
 	fmt.Println(headerBytes)
 
 	return headerBytes
 }
 
-// NullBody IS A NULL BODY THAT'S JUST 4 NULL BYTES
-type NullBody struct {
+// Unmarshall WILL ATTEMPT TO UNMARSHALL THE BYTES INTO THE REQUEST MESSAGE. IF THERE'S AN ERROR, THE MESSAGE WILL BE NIL
+func (r *RequestMessage) Unmarshall(data []byte) {
+
+	// IF THE MESSAGE IS MALFORMED, RECOVER AND DON'T DO ANYTHING
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic while unmarshalling: ", r)
+			r = nil
+		}
+	}()
+
+	r.Type = RequestMessageType(data[0])
+	r.SequenceNumber = data[1]
+	r.Nonce = binary.LittleEndian.Uint32(data[2:6])
+
+	// TODO DECRYPT THE BODY
+	if r.Type == ReadyForCommand {
+		r.Body = &NullRequestBody{}
+		r.Body.Unmarshall(data[6:])
+	} else {
+		// DEFAULT
+		r.Body = &NullRequestBody{}
+	}
+
+
+}
+
+// NewRequestMessage WILL BUILD A NEW RequestMessage WITH A RANDOM NONCE
+func NewRequestMessage(t RequestMessageType, seqNo uint8, body RequestMessageBody) *RequestMessage {
+	return &RequestMessage{
+		Type: t,
+		SequenceNumber: seqNo,
+		Nonce: rand.Uint32(),
+		Body: body,
+	}
+}
+
+////////////////////////////////////////////////////////////////////
+// RequestMessageBody type
+////////////////////////////////////////////////////////////////////
+
+// RequestMessageBody DEFINES AN INTERFACE FOR DIFFERENT REQUEST MESSAGE BODY TYPES
+type RequestMessageBody interface {
+	Marshall() []byte
+	Unmarshall([]byte)
+
+}
+
+// NullRequestBody IS A NULL BODY THAT'S JUST 4 NULL BYTES
+type NullRequestBody struct {
 	Data []byte
 }
 
 // Marshall WILL SERIALIZE THE NULL BODY AND RETURN IT
-func (nb NullBody) Marshall() []byte {
+func (nb *NullRequestBody) Marshall() []byte {
 	return []byte {0, 0, 0, 0}
 }
+
+// Unmarshall WILL UPDATE THE POINTER WITH THE PROPERTIES FROM THE BYTES
+func (nb *NullRequestBody) Unmarshall(b []byte) {
+	nb.Data = []byte {0, 0, 0, 0}
+}
+
---
+++ silence/request_test.go
@@ -0,0 +1,51 @@
+package silence
+
+import (
+	"bytes"
+	"testing"
+)
+
+// TESTS THE MARSHALLING OF THE MESSAGE
+func TestRequestMarshall(t *testing.T) {
+
+	// BUILD A RequestMessage AND MARSHALL IT TO BYTES
+	m := RequestMessage{
+		Type:           ReadyForCommand,
+		SequenceNumber: 1,
+		Nonce:          0x41424344,
+		Body:           &NullRequestBody{},
+	}
+	b := m.Marshall()
+
+	// BUILD WHAT WE THINK RequestMessage SHOULD LOOK LIKE AND MAKE SURE THEY MATCH
+	c := []byte {0x01, 0x01, 0x44, 0x43, 0x42, 0x41, 0x00, 0x00, 0x00, 0x00}
+	if bytes.Compare(b, c) != 0 {
+		t.Fatalf("Serializing request failed: expected %v amd got %v\n", c, b)
+	}
+
+}
+
+func TestRequestUnmarshall(t* testing.T) {
+	// BUILD A RequestMessage AND MARSHALL IT TO BYTES
+	m := RequestMessage{
+		Type:           ReadyForCommand,
+		SequenceNumber: 1,
+		Nonce:          0x41424344,
+		Body:           &NullRequestBody{},
+	}
+	b := m.Marshall()
+
+	// BUILD A SECOND RequestMessage AND UNMARSHALL THE FIRST ONE INTO IT
+	m2 := RequestMessage{}
+	m2.Unmarshall(b)
+	if m.Type != m2.Type {
+		t.Fatalf("Message Types don't match")
+	}
+	if m.SequenceNumber != m2.SequenceNumber {
+		t.Fatalf("Message sequence numbers don't match")
+	}
+	if m.Nonce != m2.Nonce {
+		t.Fatalf("Message nonces don't match!")
+	}
+
+}
