c9e6e5a92d10c38232eff9e5e026d3293b6291f7
Kyle Mistele <kyle@mistele.com>
2021-04-29 11:56:21 -0500

    refactoring

---
+++ implant/transport.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/k-mistele/silence/silence/transport"
+
+// send AN APPLICATION LAYER MESSAGE, FRAGMENTING IF NECESSARY.
+
+func send(messageBytes []byte) error {
+
+}
+
+func listen() []byte {
+
+}
+
+func newDatagram([]byte) *transport.Datagram
--- silence/transport/server.go
+++
@@ -1,6 +0,0 @@
-package transport
-
-// TransportServer SHOULD RUN ON THE SERVER
-type TransportServer struct {
-	ClientMap		map[uint16] string
-}
